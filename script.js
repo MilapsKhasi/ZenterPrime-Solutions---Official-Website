7ce69ed2ca03ede14c3431b7910515de169d1221
MilapsKhasi <khasimilap@gmail.com>
2026-07-19 22:25:46 -0700

    Updating Pricing Section
    Divided the Pricing Section into Monthly and Yearly sections.

--- script.js
+++ script.js
@@ -11,3 +11,49 @@ function recomputeSavings() {
     const computedSavings = (billCount * 0.15).toFixed(1);
     hoursDisplay.textContent = computedSavings;
 }
+
+// Toggle Pricing
+const monthlyBtn = document.getElementById("monthlyBtn");
+const yearlyBtn = document.getElementById("yearlyBtn");
+
+const monthlyPrices = document.querySelectorAll(".monthly-price");
+const yearlyPrices = document.querySelectorAll(".yearly-price");
+const durations = document.querySelectorAll(".billing-duration");
+
+monthlyBtn.addEventListener("click", () => {
+
+    monthlyBtn.classList.add("active");
+    yearlyBtn.classList.remove("active");
+
+    monthlyPrices.forEach(price=>{
+        price.style.display="inline";
+    });
+
+    yearlyPrices.forEach(price=>{
+        price.style.display="none";
+    });
+
+    durations.forEach(text=>{
+        text.textContent="/month";
+    });
+
+});
+
+yearlyBtn.addEventListener("click", () => {
+
+    yearlyBtn.classList.add("active");
+    monthlyBtn.classList.remove("active");
+
+    monthlyPrices.forEach(price=>{
+        price.style.display="none";
+    });
+
+    yearlyPrices.forEach(price=>{
+        price.style.display="inline";
+    });
+
+    durations.forEach(text=>{
+        text.textContent="/year";
+    });
+
+});
